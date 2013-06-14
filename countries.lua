cb79d7cdad226a23dd7af4ab0803d07b5058966b
Radamanthus Batnag <rad@batnag.org>
2013-06-15 00:27:22 +0800

    Add countries table

---
+++ countries.lua
@@ -0,0 +1,137 @@
+local countries = {
+  {
+    name = "Finland",
+    image = "finland.png",
+    width = 264,
+    height = 473,
+    targetPosition = {
+      x = 1183,
+      y = -174,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "France",
+    image = "france.png",
+    width = 342,
+    height = 318,
+    targetPosition = {
+      x = 498,
+      y = 844,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Germany",
+    image = "germany.png",
+    width = 220,
+    height = 262,
+    targetPosition = {
+      x = 748,
+      y = 612,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Great Britain",
+    image = "great_britain.png",
+    width = 228,
+    height = 415,
+    targetPosition = {
+      x = 311,
+      y = 393,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Greece",
+    image = "greece.png",
+    width = 228,
+    height = 220,
+    targetPosition = {
+      x = 1252,
+      y = 1198,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Italy",
+    image = "italy.png",
+    width = 299,
+    height = 337,
+    targetPosition = {
+      x = 842,
+      y = 1047,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Norway",
+    image = "norway.png",
+    width = 535,
+    height = 615,
+    targetPosition = {
+      x = 919,
+      y = -158,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Portugal",
+    image = "portugal.png",
+    width = 83,
+    height = 63,
+    targetPosition = {
+      x = 145,
+      y = 1151,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Spain",
+    image = "spain.png",
+    width = 325,
+    height = 245,
+    targetPosition = {
+      x = 327,
+      y = 1132,
+      dx = 40,
+      dy = 40
+    }
+  },
+  {
+    name = "Sweden",
+    image = "sweden.png",
+    width = 263,
+    height = 592,
+    targetPosition = {
+      x = 940,
+      y = -9,
+      dx = 80,
+      dy = 80
+    }
+  },
+  {
+    name = "Turkey",
+    image = "turkey.png",
+    width = 487,
+    height = 196,
+    targetPosition = {
+      x = 1644,
+      y = 1166,
+      dx = 80,
+      dy = 80
+    }
+  }
+}
+
+return countries
+
